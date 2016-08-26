44c62412e9c5a53bb5e6996814e6fd5ba1383ad3
cstrong <cstrong@arielpartners.com>
2016-08-26 19:14:12 -0400

    try to add specflow and change output to dll for .NET 4.6.1 seed

---
+++ HighFive.Server.SpecFlow/StepDefinitions/Recognitions/RecognitionFeedSteps.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HighFive.Server;
+
+namespace HighFive.Server.SpecFlow.StepDefinitions
+{
+    [Binding]
+    public class RecognitionFeedSteps
+    {
+        private int result { get; set; }
+        private HighFive.Server.Program calculator = new Calculator();
+
+        [Given]
+        public void Given_I_have_entered_P0_into_the_calculator(int p0)
+        {
+            calculator.FirstNumber = p0;
+        }
+
+        [Given]
+        public void Given_I_have_also_entered_P0_into_the_calculator(int p0)
+        {
+            calculator.SecondNumber = p0;
+        }
+
+
+        [When]
+        public void When_I_press_add()
+        {
+            result = calculator.Add();
+        }
+        
+        [Then]
+        public void Then_the_result_should_be_P0_on_the_screen(int p0)
+        {
+            Assert.AreEqual(p0, result);
+        }
+    }
+}
