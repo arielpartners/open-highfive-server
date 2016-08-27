e02d6c613b101de9488adc5ba699f5821008fb3b
Jeff Ardilio <jardilio@kpmg.com>
2016-08-27 16:02:12 -0400

    need to run dotnet restore on new projects

--- deploy.cmd
+++ deploy.cmd
@@ -67,7 +67,9 @@ SET MSBUILD_PATH=%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe
 echo Handling ASP.NET Core Web Application deployment.
 
 :: 1. Restore nuget packages
-call :ExecuteCmd nuget.exe restore "HighFive.Server\project.json" -packagesavemode nuspec
+call :ExecuteCmd dotnet restore "HighFive.Server\project.json" -packagesavemode nuspec
+call :ExecuteCmd dotnet restore "HighFive.Server.Web\project.json" -packagesavemode nuspec
+call :ExecuteCmd dotnet restore "HighFive.Server.Services\project.json" -packagesavemode nuspec
 IF !ERRORLEVEL! NEQ 0 goto error
 
 :: 2. Build and publish
