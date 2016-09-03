c1b6cfc756a538c1294b4659351c7580bbaec137
Craeg Strong <cstrong@arielpartners.com>
2016-09-02 20:23:48 -0400

    fixed 401 unauthorized response from login for bad username / password (#58)
    * fxcop fixes
    
    * fixed test errors and fixed backwards assertion
    
    * fixed invalid password 401 instead of 401

--- HighFive.Server/Startup.cs
+++ HighFive.Server/Startup.cs
@@ -64,14 +64,9 @@ namespace HighFive.Server
                 {
                     OnRedirectToLogin = async ctx =>
                     {
-                        if (ctx.Request.Path.StartsWithSegments("/api") &&
-                        ctx.Response.StatusCode == 200)
+                        if (ctx.Request.Path.StartsWithSegments("/api") && ctx.Response.StatusCode == 200)
                         {
                             ctx.Response.StatusCode = 401;
-                        }
-                        else
-                        {
-                            ctx.Response.Redirect(ctx.RedirectUri);
                         }
                         await Task.Yield();
                     }
